b05e3df5652af11e7b5ccd2a20b6fa3afc4a7166
Anonymous <anonymous@localhost.localdomain>
2016-09-20 23:59:44 +0530

    Imdb Api Init

--- api/controllers/ImdbController.js
+++ api/controllers/ImdbController.js
@@ -4,8 +4,31 @@
  * @description :: Server-side logic for managing imdbs
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var imdb = require('imdb-api');
 
 module.exports = {
-	
+	index: function(req,res){
+        sails.log.info(`${req.method}: ${req.path}`);
+        Imdb.find().then(function(response){
+            res.ok(response);
+        });
+    },
+    add: function(req,res){
+        const id = req.param('id');
+        if(!id){
+            res.badRequest('Parameter Missing');
+        }else{
+            imdb.getReq({id: id},function(err,things){
+                if(err){
+                    console.log(err);
+                    res.notFound();
+                }else{
+                    Imdb.findOrCreate({imdbid: id},{things}).then(function(response){
+                        res.ok(response);
+                    });
+                }
+            });
+        }
+    },
 };
 
